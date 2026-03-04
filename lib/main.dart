9f270ad66ed0032f5658d674b243e59fe35ae839
Naufal <naufalchumaidi001@gmail.com>
2026-03-04 11:38:23 +0800

    tambah foto

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Belajar Flutter'),
     );
   }
 }
@@ -106,7 +106,8 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            const Text('Ahmad Naufal Chumaidi'),
+            const Text('3123521058 '),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
